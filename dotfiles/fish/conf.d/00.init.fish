bf116f2981e7fc688c5ef698031d1e68ff325015
Michael Donat <michael.donat@me.com>
2024-03-08 20:12:00 +0000

    update

--- dotfiles/fish/conf.d/00.init.fish
+++ dotfiles/fish/conf.d/00.init.fish
@@ -7,3 +7,18 @@ function e
 end
 
 source "$(brew --prefix)/share/google-cloud-sdk/path.fish.inc"
+set -x GOPATH $HOME/go
+set -x GOROOT "$(brew --prefix golang)/libexec"
+set PATH $GOPATH/bin $GOROOT/bin $PATH
+
+alias mm="go run mongo-mirror.go --config-file=/Users/mikey/.mongo-mirror/config.dev.toml"
+
+if test -d (brew --prefix)"/share/fish/completions"
+    set -p fish_complete_path (brew --prefix)/share/fish/completions
+end
+
+if test -d (brew --prefix)"/share/fish/vendor_completions.d"
+    set -p fish_complete_path (brew --prefix)/share/fish/vendor_completions.d
+end
+
+set -x REDPANDA_BROKERS redpanda-broker.lab.donat.im:9092
